5d4a14e42e6ff596d269549fa05f2bc65a91ac19
bluewell-y <aoiy04414@gmail.com>
2026-06-09 00:44:17 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -25,7 +25,7 @@ class RoomsController < ApplicationController
   private
 
   def room_params
-    params.require(:room).permit(:name, usr_ids: [])
+    params.require(:room).permit(:name, user_ids: [])
   end
 
 end
